382ef886963d62f7c1f309db3c1c891dfe3f98fc
huihongsanlao <chenyangxiaoyue@hotmail.com>
2023-02-05 18:33:37 +0800

    stream测试

---
+++ trainingship/src/test/java/StreamTest.java
@@ -0,0 +1,40 @@
+import com.alibaba.fastjson.JSONObject;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+
+import pojo.Weapon;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamTest {
+
+    @Test
+    public void  test(){
+        System.out.println(1111111);
+    }
+    /**
+     * 类型转换 List<Map<String, Object>> -> List<BlackProductVo>
+     */
+    @Test
+    public void AList2BList() {
+        List<Map<String, Object>> list = Lists.newArrayList(new HashMap<>());
+            List<Weapon> blackProductVoList = list.stream().map((item) -> {
+                Weapon blackProductVo = new Weapon();
+                blackProductVo.setCode("");
+            return blackProductVo;
+        }).collect(Collectors.toList());
+
+        System.out.println(JSONObject.toJSONString(blackProductVoList));
+    }
+
+    /**
+     * 综合案例
+     */
+    public void integratedCase(String str){
+
+    }
+}
---
+++ trainingship/src/test/java/pojo/Weapon.java
@@ -0,0 +1,50 @@
+package pojo;
+
+public class Weapon {
+
+    private String Code;
+    private String name;
+    private String type;
+    private String weight;
+    private String blone;
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getBlone() {
+        return blone;
+    }
+
+    public void setBlone(String blone) {
+        this.blone = blone;
+    }
+}
